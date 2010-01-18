9328305cfb7cd48901d126aa71d57724f977858a
Brian S. O'Neill <bronee@gmail.com>
2010-01-18 19:07:30 +0000

    Fix unit test.

--- src/test/java/com/amazon/carbonado/RemoteTest.java
+++ src/test/java/com/amazon/carbonado/RemoteTest.java
@@ -40,140 +40,140 @@ import com.amazon.carbonado.*;
  */
 public class RemoteTest {
     public static void main(String[] args) {
-	org.junit.runner.JUnitCore.main(RemoteTest.class.getName());
+        org.junit.runner.JUnitCore.main(RemoteTest.class.getName());
     }
 
     @Test
     public void remoteBasicTest() throws Exception {
-	Session[] sessionPair = new Environment().newSessionPair();
-	sessionPair[0].send("hi");
-	assertEquals("hi", sessionPair[1].receive());
+        Session[] sessionPair = new Environment().newSessionPair();
+        sessionPair[0].send("hi");
+        assertEquals("hi", sessionPair[1].receive());
     }
 
     @Test
     public void mapRepoInsertTest() {
-	try{
-	    Repository repo = com.amazon.carbonado.repo.map.MapRepositoryBuilder.newRepository();
-	    
-	    Session[] pair = new Environment().newSessionPair();
-	    pair[0].send(RemoteRepositoryServer.from(repo));
-	    RemoteRepository remoteRepo = (RemoteRepository) pair[1].receive();
-	    
-	    assertTrue(!(remoteRepo instanceof RemoteRepositoryServer));
+        try{
+            Repository repo = com.amazon.carbonado.repo.map.MapRepositoryBuilder.newRepository();
+            
+            Session[] pair = new Environment().newSessionPair();
+            pair[0].send(RemoteRepositoryServer.from(repo));
+            RemoteRepository remoteRepo = (RemoteRepository) pair[1].receive();
+            
+            assertTrue(!(remoteRepo instanceof RemoteRepositoryServer));
 
-	    Repository clientRepo = ClientRepository.from(remoteRepo);
-	    assertTrue(!(clientRepo instanceof RemoteRepository));
-	    assertTrue(clientRepo instanceof ClientRepository);
-	    
-	    Storage<StorableTestBasic> storage = repo.storageFor(StorableTestBasic.class);
-	    StorableTestBasic stb = storage.prepare();
-	    stb.setId(2);
-	    if (!stb.tryLoad()) {
-		stb.setStringProp("world");
-		stb.setIntProp(321);
-		stb.setLongProp(313244232323432L);
-		stb.setDoubleProp(1.423423);
-		stb.tryInsert();
-	    }
-	    
-	    Storage <StorableTestBasic> clientstorage = clientRepo.storageFor(StorableTestBasic.class);
-	    assertTrue(!(repo.equals(clientRepo)));
-	    StorableTestBasic stb1 = clientstorage.prepare();
-	    stb1.setId(2);
-	    assertTrue(stb1.tryLoad());
-	    assertEquals("world", stb1.getStringProp());
-	    assertEquals(321, stb1.getIntProp());
-	    assertEquals(313244232323432L, stb1.getLongProp());
-	    assertEquals(1.423423, stb1.getDoubleProp());
-	    
-	    /////////////////////////
-	    stb = storage.prepare();
-	    stb.setId(3);
-	    if (!stb.tryLoad()) {
-		stb.setStringProp("hello");
-		stb.setIntProp(1);
-		stb.setLongProp(3L);
-		stb.setDoubleProp(1.4);
-		stb.tryInsert();
-	    }	
-	    
-	    stb1 = clientstorage.prepare();
-	    stb1.setId(3);
-	    assertTrue(stb1.tryLoad());
-	    assertEquals("hello", stb1.getStringProp());
+            Repository clientRepo = ClientRepository.from(remoteRepo);
+            assertTrue(!(clientRepo instanceof RemoteRepository));
+            assertTrue(clientRepo instanceof ClientRepository);
+            
+            Storage<StorableTestBasic> storage = repo.storageFor(StorableTestBasic.class);
+            StorableTestBasic stb = storage.prepare();
+            stb.setId(2);
+            if (!stb.tryLoad()) {
+                stb.setStringProp("world");
+                stb.setIntProp(321);
+                stb.setLongProp(313244232323432L);
+                stb.setDoubleProp(1.423423);
+                stb.tryInsert();
+            }
+            
+            Storage <StorableTestBasic> clientstorage = clientRepo.storageFor(StorableTestBasic.class);
+            assertTrue(!(repo.equals(clientRepo)));
+            StorableTestBasic stb1 = clientstorage.prepare();
+            stb1.setId(2);
+            assertTrue(stb1.tryLoad());
+            assertEquals("world", stb1.getStringProp());
+            assertEquals(321, stb1.getIntProp());
+            assertEquals(313244232323432L, stb1.getLongProp());
+            assertTrue(1.423423 == stb1.getDoubleProp());
+            
+            /////////////////////////
+            stb = storage.prepare();
+            stb.setId(3);
+            if (!stb.tryLoad()) {
+                stb.setStringProp("hello");
+                stb.setIntProp(1);
+                stb.setLongProp(3L);
+                stb.setDoubleProp(1.4);
+                stb.tryInsert();
+            }   
+            
+            stb1 = clientstorage.prepare();
+            stb1.setId(3);
+            assertTrue(stb1.tryLoad());
+            assertEquals("hello", stb1.getStringProp());
 
-	    ///////////////////////
-	    stb1 = clientstorage.prepare();
-	    stb1.setId(2);
-	    assertTrue(stb1.tryLoad());
-	    stb1.setStringProp("new world");
-	    assertTrue(stb1.tryUpdate());
-	    
-	    stb = storage.prepare();
-	    stb.setId(2);
-	    assertTrue(stb.tryLoad());
-	    assertEquals("new world", stb.getStringProp());
-	} catch (Exception e) {
-	    assertTrue(false);
-	}
+            ///////////////////////
+            stb1 = clientstorage.prepare();
+            stb1.setId(2);
+            assertTrue(stb1.tryLoad());
+            stb1.setStringProp("new world");
+            assertTrue(stb1.tryUpdate());
+            
+            stb = storage.prepare();
+            stb.setId(2);
+            assertTrue(stb.tryLoad());
+            assertEquals("new world", stb.getStringProp());
+        } catch (Exception e) {
+            assertTrue(false);
+        }
     }
 
     @Test
     public void anotherTest() throws Exception {
-	Repository repo = com.amazon.carbonado.repo.map.MapRepositoryBuilder.newRepository();
-	    
-	Session[] pair = new Environment().newSessionPair();
-	pair[0].send(RemoteRepositoryServer.from(repo));
+        Repository repo = com.amazon.carbonado.repo.map.MapRepositoryBuilder.newRepository();
+            
+        Session[] pair = new Environment().newSessionPair();
+        pair[0].send(RemoteRepositoryServer.from(repo));
         RemoteRepository remoteRepo = (RemoteRepository) pair[1].receive();
     }
 
     @Test
     public void transactionsTest() throws Exception {
-	Repository repo = com.amazon.carbonado.repo.map.MapRepositoryBuilder.newRepository();
-	Session[] pair = new Environment().newSessionPair();
-	pair[0].send(RemoteRepositoryServer.from(repo));
-	RemoteRepository remoteRepo = (RemoteRepository) pair[1].receive();
-	final Repository clientRepo = ClientRepository.from(remoteRepo);
+        Repository repo = com.amazon.carbonado.repo.map.MapRepositoryBuilder.newRepository();
+        Session[] pair = new Environment().newSessionPair();
+        pair[0].send(RemoteRepositoryServer.from(repo));
+        RemoteRepository remoteRepo = (RemoteRepository) pair[1].receive();
+        final Repository clientRepo = ClientRepository.from(remoteRepo);
 
-	final Storage<StorableTestBasic> clientStorage = clientRepo.storageFor(StorableTestBasic.class);
-	StorableTestBasic stb = clientStorage.prepare();
-	stb.setId(2);
-	stb.setStringProp("world");
-	stb.setIntProp(321);
-	stb.setLongProp(313244232323432L);
-	stb.setDoubleProp(1.423423);
-	stb.insert();
-	Transaction txn = clientRepo.enterTransaction();
-	try {
-	    stb.setStringProp("world1");
-	    stb.update();
-	    Thread otherThread = new Thread() {
-		    public void run() {
-			Transaction txn1 = 
-			    clientRepo.enterTransaction(IsolationLevel.READ_COMMITTED);
-			try {
-			    StorableTestBasic b = clientStorage.prepare();
-			    b.setId(2);
-			    try {
-				isSet = b.tryLoad();
-				newTransactionValue = b.getStringProp();
-			    } finally {
-				txn1.exit();
-			    }
-			} catch (Exception e) {
-			    isSet = false;
-			}
-		    }
-		};
-	    otherThread.start();
-	    otherThread.join();
-	    // assertEquals(true, isSet);
-	    // assertEquals("world1", newTransactionValue);
+        final Storage<StorableTestBasic> clientStorage = clientRepo.storageFor(StorableTestBasic.class);
+        StorableTestBasic stb = clientStorage.prepare();
+        stb.setId(2);
+        stb.setStringProp("world");
+        stb.setIntProp(321);
+        stb.setLongProp(313244232323432L);
+        stb.setDoubleProp(1.423423);
+        stb.insert();
+        Transaction txn = clientRepo.enterTransaction();
+        try {
+            stb.setStringProp("world1");
+            stb.update();
+            Thread otherThread = new Thread() {
+                    public void run() {
+                        Transaction txn1 = 
+                            clientRepo.enterTransaction(IsolationLevel.READ_COMMITTED);
+                        try {
+                            StorableTestBasic b = clientStorage.prepare();
+                            b.setId(2);
+                            try {
+                                isSet = b.tryLoad();
+                                newTransactionValue = b.getStringProp();
+                            } finally {
+                                txn1.exit();
+                            }
+                        } catch (Exception e) {
+                            isSet = false;
+                        }
+                    }
+                };
+            otherThread.start();
+            otherThread.join();
+            // assertEquals(true, isSet);
+            // assertEquals("world1", newTransactionValue);
 
-	    txn.commit();
-	} finally {
-	    txn.exit();
-	}
+            txn.commit();
+        } finally {
+            txn.exit();
+        }
     }
 
     private volatile boolean isSet = true;
@@ -181,61 +181,61 @@ public class RemoteTest {
 
     @Test
     public void transactionsAttachDetachTest() throws Exception {
-	Repository repo = com.amazon.carbonado.repo.map.MapRepositoryBuilder.newRepository();
-	Session[] pair = new Environment().newSessionPair();
-	pair[0].send(RemoteRepositoryServer.from(repo));
-	RemoteRepository remoteRepo = (RemoteRepository) pair[1].receive();
-	final Repository clientRepo = ClientRepository.from(remoteRepo);
+        Repository repo = com.amazon.carbonado.repo.map.MapRepositoryBuilder.newRepository();
+        Session[] pair = new Environment().newSessionPair();
+        pair[0].send(RemoteRepositoryServer.from(repo));
+        RemoteRepository remoteRepo = (RemoteRepository) pair[1].receive();
+        final Repository clientRepo = ClientRepository.from(remoteRepo);
 
-	final Storage<StorableTestBasic> clientStorage = clientRepo.storageFor(StorableTestBasic.class);
-	StorableTestBasic stb = clientStorage.prepare();
-	stb.setId(2);
-	stb.setStringProp("world");
-	stb.setIntProp(321);
-	stb.setLongProp(313244232323432L);
-	stb.setDoubleProp(1.423423);
-	stb.insert();
-	final Transaction txn = clientRepo.enterTransaction();
-	try {
-	    Query<StorableTestBasic> query = clientStorage.query();
-	    Cursor<StorableTestBasic> cursor = query.fetch();
-	    try {
-		final Transaction txn1 = clientRepo.enterTransaction();
-		Thread otherThread = new Thread() {
-		    public void run() {
-			//	txn.attach();
-		    }
-		};
-	
-		otherThread.start();
-		otherThread.join();
-	    } catch (Exception e) {
-		assertTrue(false);
-	    }
-	} finally {
-	    txn.exit();
-	}
+        final Storage<StorableTestBasic> clientStorage = clientRepo.storageFor(StorableTestBasic.class);
+        StorableTestBasic stb = clientStorage.prepare();
+        stb.setId(2);
+        stb.setStringProp("world");
+        stb.setIntProp(321);
+        stb.setLongProp(313244232323432L);
+        stb.setDoubleProp(1.423423);
+        stb.insert();
+        final Transaction txn = clientRepo.enterTransaction();
+        try {
+            Query<StorableTestBasic> query = clientStorage.query();
+            Cursor<StorableTestBasic> cursor = query.fetch();
+            try {
+                final Transaction txn1 = clientRepo.enterTransaction();
+                Thread otherThread = new Thread() {
+                    public void run() {
+                        //      txn.attach();
+                    }
+                };
+        
+                otherThread.start();
+                otherThread.join();
+            } catch (Exception e) {
+                assertTrue(false);
+            }
+        } finally {
+            txn.exit();
+        }
 
     }
 
     @Test
     public void querriesTest() throws Exception { 
-	Repository repo = com.amazon.carbonado.repo.map.MapRepositoryBuilder.newRepository();
-	Session[] pair = new Environment().newSessionPair();
-	pair[0].send(RemoteRepositoryServer.from(repo));
-	RemoteRepository remoteRepo = (RemoteRepository) pair[1].receive();
-	Repository clientRepo = ClientRepository.from(remoteRepo);
+        Repository repo = com.amazon.carbonado.repo.map.MapRepositoryBuilder.newRepository();
+        Session[] pair = new Environment().newSessionPair();
+        pair[0].send(RemoteRepositoryServer.from(repo));
+        RemoteRepository remoteRepo = (RemoteRepository) pair[1].receive();
+        Repository clientRepo = ClientRepository.from(remoteRepo);
 
-	Storage<StorableTestBasic> clientStorage = clientRepo.storageFor(StorableTestBasic.class);
-	StorableTestBasic stb = clientStorage.prepare();
-	stb.setId(2);
-	if (!stb.tryLoad()) {
-	    stb.setStringProp("world");
-	    stb.setIntProp(321);
-	    stb.setLongProp(313244232323432L);
-	    stb.setDoubleProp(1.423423);
-	    stb.tryInsert();
-	}
+        Storage<StorableTestBasic> clientStorage = clientRepo.storageFor(StorableTestBasic.class);
+        StorableTestBasic stb = clientStorage.prepare();
+        stb.setId(2);
+        if (!stb.tryLoad()) {
+            stb.setStringProp("world");
+            stb.setIntProp(321);
+            stb.setLongProp(313244232323432L);
+            stb.setDoubleProp(1.423423);
+            stb.tryInsert();
+        }
 
 
     }
@@ -257,34 +257,34 @@ public class RemoteTest {
 
     @Test
     public void closingTest() throws Exception {
-	Repository repo = com.amazon.carbonado.repo.map.MapRepositoryBuilder.newRepository();
-	Session[] pair = new Environment().newSessionPair();
-	pair[0].send(RemoteRepositoryServer.from(repo));
-	RemoteRepository remoteRepo = (RemoteRepository) pair[1].receive();
-	Repository clientRepo = ClientRepository.from(remoteRepo);
+        Repository repo = com.amazon.carbonado.repo.map.MapRepositoryBuilder.newRepository();
+        Session[] pair = new Environment().newSessionPair();
+        pair[0].send(RemoteRepositoryServer.from(repo));
+        RemoteRepository remoteRepo = (RemoteRepository) pair[1].receive();
+        Repository clientRepo = ClientRepository.from(remoteRepo);
 
-	Storage<StorableTestBasic> clientStorage = clientRepo.storageFor(StorableTestBasic.class);
-	StorableTestBasic stb = clientStorage.prepare();
-	stb.setId(2);
-	if (!stb.tryLoad()) {
-	    stb.setStringProp("world");
-	    stb.setIntProp(321);
-	    stb.setLongProp(313244232323432L);
-	    stb.setDoubleProp(1.423423);
-	    stb.tryInsert();
-	}
+        Storage<StorableTestBasic> clientStorage = clientRepo.storageFor(StorableTestBasic.class);
+        StorableTestBasic stb = clientStorage.prepare();
+        stb.setId(2);
+        if (!stb.tryLoad()) {
+            stb.setStringProp("world");
+            stb.setIntProp(321);
+            stb.setLongProp(313244232323432L);
+            stb.setDoubleProp(1.423423);
+            stb.tryInsert();
+        }
 
-	clientRepo.close();
+        clientRepo.close();
 
-	Storage<StorableTestBasic> actualStorage = repo.storageFor(StorableTestBasic.class);
-	StorableTestBasic stb1 = actualStorage.prepare();
-	stb1.setId(3);
-	boolean caught = false;
-	try {
-	    stb.tryLoad();
-	} catch (IllegalStateException e) {
-	    caught = true;
-	}
-	assertTrue(caught);
+        Storage<StorableTestBasic> actualStorage = repo.storageFor(StorableTestBasic.class);
+        StorableTestBasic stb1 = actualStorage.prepare();
+        stb1.setId(3);
+        boolean caught = false;
+        try {
+            stb.tryLoad();
+        } catch (IllegalStateException e) {
+            caught = true;
+        }
+        assertTrue(caught);
     }
 }
